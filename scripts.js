a9895d392e630549046159eee5b3c89daecd4902
steklyanov <steklyanov@gmail.com>
2019-04-08 15:41:43 +0300

    save as a object

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let rbServer = new ROSLIB.Ros({
     url: 'ws://localhost:9090'
 
 });
-
+let node_array = {};
 function getNodeDetail() {
     let detailClient = new ROSLIB.Service({
         ros: rbServer,
@@ -13,95 +13,94 @@ function getNodeDetail() {
     });
     let request = new ROSLIB.ServiceRequest({});
     detailClient.callService(request, function (result) {
+        document.getElementById("node_list").innerHTML = result;
+        node_array = result;
         splitNodes(result);
     });
 }
-
 function splitNodes(result) {
-    console.log(result.nodes)
-    result.nodes.forEach(function (item, index, arr) {
-        getTopicDetail(item, index);
+    console.log(result.nodes);
+    result.nodes.forEach(function (item) {
+
+        getTopicDetail(item);
     })
 }
 
-function getTopicDetail(item, index) {
+function getTopicDetail(item) {
     let topicDetail = new ROSLIB.Service({
         ros: rbServer,
         name: 'rosapi/node_details',
         serviceType: 'rosapi/NodeDetails'
     })
-    // console.log(index)
     let request = new ROSLIB.ServiceRequest({
         node : item
     })
 
     topicDetail.callService(request, function (result) {
-        console.log('for node '+ item +' available following topics')
-        console.log(result);
-        result.publishing.forEach(function (item) {
-            getTopicMessageType(item, index)
+        node_array[item] = result;
+        result.publishing.forEach(function (elem) {
+            getTopicMessageTypeSub(elem, item)
         })
-        result.subscribing.forEach(function (item) {
-            getServiceMessageTypePub(item, index)
+        result.subscribing.forEach(function (elem) {
+            getTopicMessageTypePub(elem, item)
         })
-        result.services.forEach(function (item) {
-            getServiceMessageTypeSub(item, index)
+        result.services.forEach(function (elem) {
+            getServiceMessageType(elem, item)
         })
     })
 }
 
-function getTopicMessageType(item, index) {
+function getTopicMessageTypeSub(item, name) {
     let messageType = new ROSLIB.Service({
         ros: rbServer,
         name: 'rosapi/topic_type',
         serviceType: 'rosapi/TopicType'
     })
-    console.log(item, index)
+    // console.log(item, index)
     let request = new ROSLIB.ServiceRequest({
         topic : item
     })
     messageType.callService(request, function (result) {
-        // console.log(item);
-        // console.log(index);
-        console.log('sub'+result, index);
+        node_array[name]['subscribing'][item] = result.type;
     })
 
 }
 
-function getServiceMessageTypePub(item, index) {
+function getServiceMessageType(item, name) {
     let messageType = new ROSLIB.Service({
         ros: rbServer,
         name: 'rosapi/service_type',
         serviceType: 'rosapi/ServiceType'
     })
-    console.log(item, index)
+    // console.log(item, index)
     let request = new ROSLIB.ServiceRequest({
         service : item
     })
     messageType.callService(request, function (result) {
-        // console.log(item);
-        // console.log(index);
-        console.log('pub'+result, index);
+        node_array[name]['services'][item] = result.type;
+        document.getElementById("publisher_list").innerHTML = result;
     })
 
 }
 
-function getServiceMessageTypeSub(item, index) {
+function getTopicMessageTypePub(item, name) {
     let messageType = new ROSLIB.Service({
         ros: rbServer,
-        name: 'rosapi/service_type',
-        serviceType: 'rosapi/ServiceType'
+        name: 'rosapi/topic_type',
+        serviceType: 'rosapi/TopicType'
     })
-    console.log(item, index)
+    // console.log(item, index)
     let request = new ROSLIB.ServiceRequest({
-        service : item
+        topic : item
     })
     messageType.callService(request, function (result) {
-        // console.log(item);
-        // console.log(index);
-        console.log(result, index);
+        node_array[name]['publishing'][item] = result.type;
     })
 
+}
+
+function postNodeDetail() {
+    console.log(node_array);
 }
 
 function createForm(title, body) {
