245ed353a74b2aa4e4da3bb6550c714bb161d7a2
steklyanov <steklyanov@gmail.com>
2019-04-24 15:11:19 +0300

    did node page template

--- frontend/src/components/MainStructure.vue
+++ frontend/src/components/MainStructure.vue
@@ -23,7 +23,7 @@
       <a class="nav-link" href="#">History</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#" @click="goPage('settings')">Settings</a>
+      <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter"> <!--@click="goPage('settings')"-->Settings</a>
     </li>
     <li class="nav-item">
       <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
@@ -55,6 +55,30 @@
               <div class="card-header">
                   HEADER
               </div>
+      </div>
+    </div>
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">robot IP address</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon3">IP-address</span>
+              </div>
+              <input type="text" id="ip_address" class="form-control" value="10.0.1.7" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" onclick="change_ip()" data-dismiss="modal" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
