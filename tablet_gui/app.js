64c6c91f8e5837e799a9fa7258831f895f03176e
steklyanov <steklyanov@gmail.com>
2019-05-20 13:23:16 +0300

    del trash

--- tablet_gui/app.js
+++ tablet_gui/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/static'));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
