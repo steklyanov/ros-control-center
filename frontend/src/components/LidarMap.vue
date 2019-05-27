5cc394969521cb5d7aabaa5ea551ee780490f296
steklyanov <steklyanov@gmail.com>
2019-05-27 11:50:35 +0300

    cahnge gui, map behavior

--- frontend/src/components/LidarMap.vue
+++ frontend/src/components/LidarMap.vue
@@ -7,47 +7,40 @@
   // import createjs from "@/public/createjs.js"
   // console.log(createjs);
   // import ROS2D from "@/public/ros2d.js"
-  import NAV2D from "@/public/nav2d.min"
+  // import NAV2D from "@/public/nav2d.min"
 
 
     export default {
       name: "LidarMap",
       methods: {
         init_nav() {
-          let ros = new ROSLIB.Ros({
-            url : 'ws://localhost:9090'
-          });
-          let viewer = new ROS2D.Viewer({
-            divID : 'nav',
-            width : 350,
-            height : 400
-          });
-          let nav = NAV2D.OccupancyGridClientNav({
-            ros : ros,
-            rootObject : viewer.scene,
-            viewer : viewer,
-            serverName : '/pr2_move_base'
-          });
+
         }
       },
-      // created () {
-      // },
-      created() {
-        var scriptTag = document.createElement("script");
-        scriptTag.src = "https://code.createjs.com/1.0.0/easeljs.min.js";
-        scriptTag.id = "easel";
-        document.getElementsByTagName('head')[0].appendChild(scriptTag);
+      mounted() {
+        // var scriptTag2 = document.createElement("script");
+        // scriptTag2.src = "https://static.robotwebtools.org/ros2djs/current/ros2d.js";
+        // scriptTag2.id = "ros2d";
+        // document.getElementsByTagName('head')[0].appendChild(scriptTag2);
 
-        var scriptTag2 = document.createElement("script");
-        scriptTag2.src = "https://static.robotwebtools.org/ros2djs/current/ros2d.js";
-        scriptTag2.id = "ros2d";
-        document.getElementsByTagName('head')[0].appendChild(scriptTag2);
-
-        self.init_nav();
+        // self.init_nav();
+        console.log("started");
+        let ros = new ROSLIB.Ros({
+          url : 'ws://localhost:9090'
+        });
+        let viewer = new ROS2D.Viewer({
+          divID : 'nav',
+          width : 350,
+          height : 400
+        });
+        let nav = NAV2D.OccupancyGridClientNav({
+          ros : ros,
+          rootObject : viewer.scene,
+          viewer : viewer,
+          serverName : '/pr2_move_base'
+        });
       }
     }
-
-
 </script>
 <style scoped>
 
